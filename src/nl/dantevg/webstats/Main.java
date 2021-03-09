a4390980abbb163d18cd2da61e29c1805c39c1c5
Dantevg <dantevg006@gmail.com>
2021-03-09 11:55:36 +0100

    Use actually working thread stop method

--- src/nl/dantevg/webstats/Main.java
+++ src/nl/dantevg/webstats/Main.java
@@ -6,25 +6,39 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 public class Main extends JavaPlugin implements Runnable {
-	private int port;
+	private ServerSocket serverSocket;
 	private Thread thread;
-	private boolean active = true;
 	
 	// Gets run when the plugin is enabled on server startup
 	@Override
 	public void onEnable() {
 		// Config
 		saveDefaultConfig();
-		port = getConfig().getInt("port");
+		int port = getConfig().getInt("port");
 		
-		// Start server in a new thread, otherwise `serverSocket.accept()` will block the main thread
-		thread = new Thread(this, "WebStats");
-		thread.start();
+		try {
+			// Open server socket
+			serverSocket = new ServerSocket(port);
+			System.out.print("Web stats server started on port " + port);
+			
+			// Start server in a new thread, otherwise `serverSocket.accept()` will block the main thread
+			thread = new Thread(this, "WebStats");
+			thread.start();
+		} catch (IOException e) {
+			System.err.print("Failed to open socket: " + e.getMessage());
+		}
 	}
 	
+	// Gets run when the plugin is disabled on server stop
 	@Override
 	public void onDisable() {
-		active = false;
+		// Close socket
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			System.err.print("Failed to close socket: " + e.getMessage());
+		}
+		// Stop thread
 		try {
 			thread.join(100); // Wait max 0.1s for the thread to stop
 		} catch (InterruptedException e) {
@@ -36,17 +50,16 @@ public class Main extends JavaPlugin implements Runnable {
 	@Override
 	public void run() {
 		try {
-			ServerSocket serverSocket = new ServerSocket(port);
-			System.out.println("Web stats server started on port " + port);
-			
-			// Accept new connections
-			while (active) {
+			while (!serverSocket.isClosed()) {
+				// Accept new connections
 				// Only one connection at a time possible, I don't expect heavy traffic
 				Connection.start(serverSocket.accept());
 			}
-			
 		} catch (IOException e) {
-			System.err.println("Web stats server IO Exception: " + e.getMessage());
+			if(!serverSocket.isClosed()){
+				// Print error when the socket was not closed (otherwise just stop)
+				System.err.print("IO Exception: " + e.getMessage());
+			}
 		}
 	}
 	
