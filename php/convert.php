eafbfb33a42a33f6d37a2fcbd243febdf45f9e03
RedPolygon <mail@dantevg.nl>
2021-07-27 15:51:16 +0200

    Add 2 converters, finalise

--- php/convert.php
+++ php/convert.php
@@ -3,6 +3,23 @@
 $converters = array();
 
 $converters["json"] = function($row, $config){
+	$new_row = array();
+	$new_row[$config["key"]] = $row[$config["key"]];
+	$key = $config["columns"][0];
+	$json_data = json_decode($row[$key], true);
+	if(!$json_data){
+		echo "could not decode json data<br>";
+		return $new_row;
+	}
+	return array_merge($new_row, $json_data);
+};
+
+$converters["keyvalue"] = function($row, $config){
+	$key = $config["columns"][0];
+	$value = $config["columns"][1];
+	$row[$row[$key]] = $row[$value];
+	unset($row[$key]);
+	unset($row[$value]);
 	return $row;
 };
 
--- php/stats.php
+++ php/stats.php
@@ -4,19 +4,19 @@
 require("data.php");
 require("convert.php");
 
-function convert_table($table, $config_item, $config, $converters){
+function convert_table($table, $config_item, $converters){
 	if(!array_key_exists("convert", $config_item)) return $table;
 	foreach($config_item["convert"] as $converter){
 		if(!array_key_exists($converter, $converters)){
-			echo "no such converter: ".$converter;
+			echo "no such converter: ".$converter."<br>";
 			continue;
 		}
-		foreach($table as &$row) $row = $converters[$converter]($row, $config);
+		foreach($table as &$row) $row = $converters[$converter]($row, $config_item);
 	}
 	return $table;
 }
 
-function rename_columns($table, $config_item, $config){
+function rename_columns($table, $config_item){
 	if(!array_key_exists("renameColumns", $config_item)) return $table;
 	foreach($config_item["renameColumns"] as $from => $to){
 		foreach($table as &$row){
@@ -34,34 +34,37 @@ $config = json_decode($config_string, true);
 $main_key = "player";
 
 // Get and convert each database/table combination
-$data = array();
+$entries = array();
 foreach($config as $config_item){
 	$table = get_table($config_item["database"], $config_item["table"]);
-	$table = convert_table($table, $config_item, $config, $converters);
-	$table = rename_columns($table, $config_item, $config);
-	$data = array_merge($data, $table);
+	$table = convert_table($table, $config_item, $converters);
+	$table = rename_columns($table, $config_item);
+	$entries = array_merge($entries, $table);
 }
 
 // Transpose to players
 $players = array();
-foreach($data as $row){
+foreach($entries as $row){
 	$player = $row[$main_key];
 	$players[$player] = array_merge($players[$player] ?? array(), $row);
 	unset($players[$player][$main_key]); // Remove key from values
 }
 
 // Transpose again to stats
-$stats = array();
+$data = array();
+$data["entries"] = array();
+$data["scores"] = array();
 foreach($players as $player_name => $scores){
+	$data["entries"][] = $player_name;
 	foreach($scores as $stat_name => $stat_value){
-		if(!array_key_exists($stat_name, $stats)) $stats[$stat_name] = array();
-		$stats[$stat_name][$player_name] = $stat_value;
+		if(!array_key_exists($stat_name, $data["scores"])) $data["scores"][$stat_name] = array();
+		$data["scores"][$stat_name][$player_name] = $stat_value;
 	}
 }
 
 // Output
-// echo(json_encode($stats));
-print_r($stats);
+echo(json_encode($data));
+// print_r($data);
 
 ?>
 </pre>
