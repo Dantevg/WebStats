6ec151a810fdd1f0216fe9400e82cf42d524d600
Dantevg <dantevg006@gmail.com>
2022-01-24 18:22:34 +0100

    Split HTTPRequestHandler, send status codes on errors

---
+++ src/nl/dantevg/webstats/HTTPConnection.java
@@ -0,0 +1,62 @@
+package nl.dantevg.webstats;
+
+import com.google.common.io.ByteStreams;
+import com.sun.net.httpserver.HttpExchange;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.util.logging.Level;
+
+public class HTTPConnection {
+	private final @NotNull HttpExchange exchange;
+	
+	public HTTPConnection(@NotNull HttpExchange exchange) {
+		this.exchange = exchange;
+	}
+	
+	private void setHeaders(String contentType) {
+		exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+		exchange.getResponseHeaders().add("Content-Type", contentType + "; charset=UTF-8");
+	}
+	
+	public void send(int status, @NotNull String contentType, @NotNull String response) throws IOException {
+		setHeaders(contentType);
+		
+		// Send headers and data
+		byte[] responseBytes = response.getBytes();
+		exchange.sendResponseHeaders(status, responseBytes.length);
+		OutputStream output = exchange.getResponseBody();
+		output.write(responseBytes);
+		output.close();
+	}
+	
+	public void sendJson(@NotNull String response) throws IOException {
+		send(HttpURLConnection.HTTP_OK, "application/json", response);
+	}
+	
+	public void sendEmptyStatus(int status) throws IOException {
+		exchange.sendResponseHeaders(status, 0);
+	}
+	
+	public void sendFile(@NotNull String contentType, @NotNull String path) throws IOException {
+		InputStream input = WebStats.getPlugin(WebStats.class).getResource(path);
+		if (input == null) {
+			WebStats.logger.log(Level.WARNING, "Could not find resource " + path);
+			sendEmptyStatus(HttpURLConnection.HTTP_NOT_FOUND);
+			return;
+		}
+		
+		setHeaders(contentType);
+		
+		// Send headers and data
+		exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, input.available());
+		OutputStream output = exchange.getResponseBody();
+		ByteStreams.copy(input, output);
+		input.close();
+		output.close();
+	}
+	
+}
--- src/nl/dantevg/webstats/HTTPRequestHandler.java
+++ src/nl/dantevg/webstats/HTTPRequestHandler.java
@@ -1,93 +1,56 @@
 package nl.dantevg.webstats;
 
-import com.google.common.io.ByteStreams;
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
-import java.util.logging.Level;
 
 public class HTTPRequestHandler implements HttpHandler {
 	@Override
 	public void handle(@NotNull HttpExchange exchange) throws IOException {
-		// Only handle GET-requests
-		if (!exchange.getRequestMethod().equals("GET")) return;
+		HTTPConnection httpConnection = new HTTPConnection(exchange);
 		
+		// Only handle GET-requests
+		if (!exchange.getRequestMethod().equals("GET")) {
+			httpConnection.sendEmptyStatus(HttpURLConnection.HTTP_BAD_METHOD);
+			return;
+		}
+		
+		// No URI present
 		String path = exchange.getRequestURI().getPath();
-		if (path == null) return;
+		if (path == null) {
+			httpConnection.sendEmptyStatus(HttpURLConnection.HTTP_BAD_REQUEST);
+			return;
+		}
 		
 		switch (path) {
 			case "/stats.json":
 				InetAddress ip = exchange.getRemoteAddress().getAddress();
-				sendJson(exchange, new Gson().toJson(Stats.getAll(ip)));
+				httpConnection.sendJson(new Gson().toJson(Stats.getAll(ip)));
 				break;
 			case "/online.json":
-				sendJson(exchange, new Gson().toJson(Stats.getOnline()));
+				httpConnection.sendJson(new Gson().toJson(Stats.getOnline()));
 				break;
 			case "/index.html":
 			case "/":
-				sendFile(exchange, "text/html", "resources/index.html");
+				httpConnection.sendFile("text/html", "resources/index.html");
 				break;
 			case "/style.css":
-				sendFile(exchange, "text/css", "resources/style.css");
+				httpConnection.sendFile("text/css", "resources/style.css");
 				break;
 			case "/WebStats-dist.js":
-				sendFile(exchange, "application/javascript", "resources/WebStats-dist.js");
+				httpConnection.sendFile("application/javascript", "resources/WebStats-dist.js");
 				break;
 			default:
-				sendNotFound(exchange);
+				httpConnection.sendEmptyStatus(HttpURLConnection.HTTP_NOT_FOUND);
 				break;
 		}
 		
 		exchange.close();
-	}
-	
-	private static void setHeaders(@NotNull HttpExchange exchange, String contentType) {
-		exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
-		exchange.getResponseHeaders().add("Content-Type", contentType + "; charset=UTF-8");
-	}
-	
-	private static void send(@NotNull HttpExchange exchange, int status, @NotNull String contentType, @NotNull String response) throws IOException {
-		setHeaders(exchange, contentType);
-		
-		// Send headers and data
-		byte[] responseBytes = response.getBytes();
-		exchange.sendResponseHeaders(status, responseBytes.length);
-		OutputStream output = exchange.getResponseBody();
-		output.write(responseBytes);
-		output.close();
-	}
-	
-	private static void sendJson(@NotNull HttpExchange exchange, @NotNull String response) throws IOException {
-		send(exchange, HttpURLConnection.HTTP_OK, "application/json", response);
-	}
-	
-	private static void sendNotFound(@NotNull HttpExchange exchange) throws IOException {
-		send(exchange, HttpURLConnection.HTTP_NOT_FOUND, "text/plain", "");
-	}
-	
-	private static void sendFile(@NotNull HttpExchange exchange, @NotNull String contentType, @NotNull String path) throws IOException {
-		InputStream input = WebStats.getPlugin(WebStats.class).getResource(path);
-		if (input == null) {
-			WebStats.logger.log(Level.WARNING, "Could not find resource " + path);
-			sendNotFound(exchange);
-			return;
-		}
-		
-		setHeaders(exchange, contentType);
-		
-		// Send headers and data
-		exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, input.available());
-		OutputStream output = exchange.getResponseBody();
-		ByteStreams.copy(input, output);
-		input.close();
-		output.close();
 	}
 	
 }
