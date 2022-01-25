516674254e510e579c2fbbbed5f8294dc4b25891
Dantevg <dantevg006@gmail.com>
2022-01-25 19:15:42 +0100

    Read response, add testing code

--- src/nl/dantevg/webstats/discord_webhook/DiscordWebhook.java
+++ src/nl/dantevg/webstats/discord_webhook/DiscordWebhook.java
@@ -4,10 +4,10 @@ import com.google.gson.Gson;
 import nl.dantevg.webstats.WebStats;
 
 import javax.net.ssl.HttpsURLConnection;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.logging.Level;
 
 public class DiscordWebhook {
 	private final URL url;
@@ -22,7 +22,20 @@ public class DiscordWebhook {
 	}
 	
 	public void activate() {
+		// TODO: remove, for testing!
+		Embed embed = new Embed();
+		embed.addField(new Embed.EmbedField("Player", "RedPolygon\nNotch", true));
+		embed.addField(new Embed.EmbedField("Deaths", "17\n", true));
+		embed.addField(new Embed.EmbedField("Mine Diamond", "64\n256", true));
 		
+		Message message = new Message();
+		message.addEmbed(embed);
+		
+		try {
+			send(message);
+		} catch (IOException e) {
+			WebStats.logger.log(Level.WARNING, "Could not send webhook message", e);
+		}
 	}
 	
 	private void send(Message message) throws IOException {
@@ -35,6 +48,13 @@ public class DiscordWebhook {
 		try (OutputStream output = conn.getOutputStream()) {
 			output.write(new Gson().toJson(message).getBytes());
 		}
+		
+		InputStream input = conn.getInputStream();
+		WebStats.logger.log(Level.INFO, "Response:");
+		new BufferedReader(new InputStreamReader(input)).lines()
+				.forEach((line) -> WebStats.logger.log(Level.INFO, line));
+		input.close();
+		conn.disconnect();
 	}
 	
 }
