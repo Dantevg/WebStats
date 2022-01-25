6e29911c4166392f4b0e33db93c97f3bc1badce6
Dantevg <dantevg006@gmail.com>
2022-01-25 18:41:38 +0100

    Add Message class

---
+++ src/nl/dantevg/webstats/discord_webhook/Message.java
@@ -0,0 +1,29 @@
+package nl.dantevg.webstats.discord_webhook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Message {
+	String content;
+	String username;
+	String avatar_url;
+	boolean tts; // text-to-speech
+	List<Embed> embeds;
+	
+	public Message(String content, String username, String avatar_url) {
+		this.content = content;
+		this.username = username;
+		this.avatar_url = avatar_url;
+	}
+	
+	public Message(String username, String avatar_url) {
+		this.username = username;
+		this.avatar_url = avatar_url;
+	}
+	
+	public void addEmbed(Embed embed) {
+		if (embeds == null) embeds = new ArrayList<>();
+		embeds.add(embed);
+	}
+	
+}
