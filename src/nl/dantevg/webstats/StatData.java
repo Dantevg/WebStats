e82a49379595c2445e3dd743c44dbdf98f470a71
RedPolygon <mail@dantevg.nl>
2022-05-07 10:26:54 +0200

    Convert Guava Table to Map-in-Map before JSON serialization

--- src/nl/dantevg/webstats/StatData.java
+++ src/nl/dantevg/webstats/StatData.java
@@ -2,9 +2,14 @@ package nl.dantevg.webstats;
 
 import com.google.common.collect.Table;
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -34,6 +39,7 @@ public class StatData {
 	public static class Stats {
 		public Set<String> entries;
 		public List<String> columns;
+		@JsonAdapter(TableAdapter.class)
 		public Table<String, String, String> scores;
 		
 		public Stats(@NotNull EntriesScores entriesScores, List<String> columns) {
@@ -45,6 +51,15 @@ public class StatData {
 		public Stats(@NotNull EntriesScores entriesScores) {
 			entries = entriesScores.entries;
 			scores = entriesScores.scores;
+		}
+		
+		// This is necessary because Gson does not serialize Guava Tables correctly,
+		// convert Table to Map-in-Map first
+		private static class TableAdapter implements JsonSerializer<Table<?, ?, ?>> {
+			@Override
+			public JsonElement serialize(Table<?, ?, ?> table, Type type, JsonSerializationContext context) {
+				return context.serialize(table.rowMap());
+			}
 		}
 		
 	}
