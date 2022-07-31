925d612dec09d6bd0f0449f6f9d85a4fbebfedd7
RedPolygon <dantevg006@gmail.com>
2022-07-31 17:19:02 +0200

    Convert to typescript

---
+++ web/src/Connection.ts
@@ -0,0 +1,31 @@
+export default class Connection {
+	all: string
+	scores: string
+	online: string
+	
+	constructor({all, scores, online}){
+		this.all    = all
+		this.scores = scores
+		this.online = online
+	}
+	
+	static json(ip, port){
+		const baseURL = `http://${ip}:${port}`
+		return new Connection({
+			all: baseURL + "/stats.json",
+			scores: baseURL + "/scoreboard.json",
+			online: baseURL + "/online.json"
+		})
+	}
+	
+	getStats      = async () => {
+		if(this.all){
+			return await (await fetch(this.all)).json()
+		}else{
+			const [online, scoreboard] = await Promise.all([this.getOnline(), this.getScoreboard()])
+			return {online, scoreboard}
+		}
+	}
+	getScoreboard = () => fetch(this.scores).then(response => response.json())
+	getOnline     = () => fetch(this.online).then(response => response.json())
+}
