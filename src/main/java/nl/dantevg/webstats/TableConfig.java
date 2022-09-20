3d3fbe78d1795f38b90c3a55fcd7a47b5c9c82c2
RedPolygon <dantevg006@gmail.com>
2022-09-20 17:42:43 +0200

    Fix @Nullable annotations for TableConfig

--- src/main/java/nl/dantevg/webstats/TableConfig.java
+++ src/main/java/nl/dantevg/webstats/TableConfig.java
@@ -1,16 +1,20 @@
 package nl.dantevg.webstats;
 
 import com.google.gson.Gson;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
 public class TableConfig {
-	public String name;
-	public List<String> columns;
-	public String sortBy;
-	public Boolean sortDescending;
+	public @Nullable String name;
+	public @Nullable List<String> columns;
+	public @Nullable String sortBy;
+	public @Nullable Boolean sortDescending;
 	
-	public TableConfig(String name, List<String> columns, String sortBy, Boolean sortDescending) {
+	public TableConfig(@Nullable String name,
+	                   @Nullable List<String> columns,
+	                   @Nullable String sortBy,
+	                   @Nullable Boolean sortDescending) {
 		this.name = name;
 		this.columns = columns;
 		this.sortBy = sortBy;
